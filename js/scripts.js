0f01e9e44c8cca3d76493acaf48cfa11b8683480
Cat-Denton <willwdenton@gmail.com>
2021-02-26 23:52:54 -0800

    Add .each loop to take information from pizzaBuilder form and build a new pizza object.

--- js/scripts.js
+++ js/scripts.js
@@ -22,11 +22,23 @@ Pizza.prototype.pizzaPrice = function() {
     price += 18;
   };
   const pizzaToppingsKeys = Object.keys(pizza.toppings);
-
   pizzaToppingsKeys.forEach(function(key) {
     total += pizza.toppings[key].price
   });
-
   return total;
-}
+};
 
+$(document).ready(function() {
+  $("form#pizzaBuilder").submit(function(event) {
+    
+    let pizza = new Pizza($("#size").val())
+    $("input:checkbox[name=pizza-topping]:checked").each(function(){
+      let toppingString = $(this).val().toString();
+      const toppingArray = toppingString.split(",");
+      let newTopping = new Topping(toppingArray[0],toppingArray[1]);
+      pizza.addTopping(newTopping);      
+    });
+    console.log(pizza)
+    event.preventDefault();
+  });
+});
